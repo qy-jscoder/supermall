b4dddf79cd50a1738018ad281254acdaf49b9a06
maiden-killer <904874183@qq.com>
2021-05-13 02:50:53 +0800

    v3

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export function request(config){
     const instance = axios.create({
-        baseURL: "http://152.136.185.210:7878/api/m5",
+        //想要接口的，请自己添加coderwhy老师微信购买
+        //讲诚信，答应了不向外传接口
+        baseURL: "http://xxxxxx/xxxxx/xxx",
         timeout: 5000
       })
       //请求拦截
